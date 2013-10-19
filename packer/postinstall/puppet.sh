1ca9e8fe6f980015946879cfa0acf8ec348c9c61
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-10-19 17:25:17 +0900

    Add arch variable

--- packer/postinstall/puppet.sh
+++ packer/postinstall/puppet.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ARCH=$(uname -i)
+
 if [ -f /etc/system-release ]; then
   rpm -ivh "http://yum.puppetlabs.com/el/6/products/${ARCH}/puppetlabs-release-6-7.noarch.rpm"
   yum -y install puppet facter
