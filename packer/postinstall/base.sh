83821e0150732f15743028a32b8b85ee752542b9
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-10-19 15:37:51 +0900

    Remove epel repository

--- packer/postinstall/base.sh
+++ packer/postinstall/base.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 # Install basic packages
 
-if [ -f /etc/redhat-release ]; then
-  # Add EPEL repository
-  cat > /etc/yum.repos.d/epel.repo << EOM
-[epel]
-name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
-enabled=1
-gpgcheck=0
-EOM
-
+if [ -f /etc/system-release ]; then
   yum -y update
   yum -y groupinstall 'Development Tools'
   yum -y install kernel-headers-$(uname -r) kernel-devel-$(uname -r)
   yum -y install zlib-devel openssl-devel readline-devel sqlite-devel
-  yum -y install wget perl nfs-utils bind-utils
+  yum -y install wget curl perl nfs-utils bind-utils
 fi
 
 if [ -f /etc/debian_version ]; then
@@ -23,6 +14,6 @@ if [ -f /etc/debian_version ]; then
   apt-get -y upgrade
   apt-get -y install build-essential
   apt-get -y linux-headers-$(uname -r)
-  apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev wget curl
-  apt-get clean
+  apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
+  apt-get -y install wget curl
 fi
