32ed779fcb9b8fce2b3017aee17fd08d170c5331
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-10-19 15:38:51 +0900

    Use puppetlabs-release rpm

--- packer/postinstall/puppet.sh
+++ packer/postinstall/puppet.sh
@@ -1,26 +1,10 @@
 #!/bin/sh
 
-if [ -f /etc/redhat-release ]; then
-  # Add Puppet repository
-  cat > /etc/yum.repos.d/puppetlabs.repo << EOM
-[puppetlabs]
-name=puppetlabs
-baseurl=http://yum.puppetlabs.com/el/6/products/\$basearch
-enabled=1
-gpgcheck=0
-EOM
-
-  # Add Puppet dependencies repository
-  #   See issue: https://github.com/jedi4ever/veewee/issues/696
-  cat > /etc/yum.repos.d/puppetdeps.repo << EOM
-[puppetdeps]
-name=puppetdeps
-baseurl=http://yum.puppetlabs.com/el/6/dependencies/\$basearch
-enabled=1
-gpgcheck=0
-EOM
-
+if [ -f /etc/system-release ]; then
+  rpm -ivh "http://yum.puppetlabs.com/el/6/products/${ARCH}/puppetlabs-release-6-7.noarch.rpm"
   yum -y install puppet facter
-elif [ -f /etc/debian_version ]; then
+fi
+
+if [ -f /etc/debian_version ]; then
   apt-get -y install puppet facter
 fi
